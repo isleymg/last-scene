1c0797f82abf8c0005da7a40754c7f11f08b3cc2
Isley M. Gao <jiayueg@uci.edu>
2017-04-08 19:28:06 -0700

    Delete config.js

--- src/config.js
+++
@@ -1,6 +0,0 @@
-var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAD6WeMgeaoBADhCSLZAZCrj7UrXtxoImeq8aPGH8Gi7aboAWZAS5JHZBq3HYxhPvaZBwaozs8ZAZBvGp2XIEBQZBiaocDBbsoIN9hbXqghrA3VvcljV5WVdxXXzUVAnRhwZCDn71LJg0yqDnZCrZBk4AG7B3WqkbBU495h7GzFgl72bAZDZD';
-
-module.exports = {
-  messengerPageToken: messengerPageToken,
-  messengerVerifyToken: 'TOKEN'
-};
