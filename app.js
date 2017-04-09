56ff4235b7ddcce82aa10ea3b42487d2967613ee
yehkatie <katiesyeh@gmail.com>
2017-04-08 22:31:03 -0700

    Able to do latlong search with timeframe on flickr and retrieve URLS

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs ');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+        api_key: "65655937b6264a3872c5a808c754747c",
+        secret: "ba5154c1f75e6918"
+    };
+
+/*Flickr.authenticate(flickrOptions, function(error, flickr) {
+    // we can now use "flickr" as our API object
+}*/
+
+Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    // we can now use "flickr" as our API object,
+    // but we can only call public methods and access public data
+    // if (error) {
+    //     throw new Error(error);
+    // }
+    console.log("here");
+    flickr.photos.search({
+        // text: "red+panda"
+        lat: 34.9683009,
+        lon: 135.7727,
+        min_taken_date: 1491000000,
+        max_taken_date: 1491713952
+
+
+    }, function(err, result) {
+        console.log("**", result.photos.photo);
+        var pictureURLS = [];
+        for (i=0;i<result.photos.photo.length; i++) {
+            pictureURLS[i] = "https://farm" + result.photos.photo[i].farm.toString() +
+                ".staticflickr.com/" + result.photos.photo[i].server +"/"+ result.photos.photo[i].id +
+                "_" + result.photos.photo[i].secret+".jpg";
+            console.log(pictureURLS[i]);
+
+        }
+
+        if(err) { throw new Error(err); }
+        // do something with result
+        console.log("blah");
+    });
+
+
+});
+
+
 
 module.exports = router;
