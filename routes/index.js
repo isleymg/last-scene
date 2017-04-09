60c7eafbad826250960f380a62fcf3834dd83a22
alyssalingad <alyssalingad@gmail.com>
2017-04-09 00:57:10 -0700

    added ability to choose time and date for last scene

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,42 @@ var google_maps_key = 'AIzaSyD1GPoQj-oO50V8JJJ01xRNVxwYb2WyQoU';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var dateObj = new Date();
+    var month = dateObj.getMonth() + 1; //months from 1-12
+    var day = dateObj.getDate();
+    var year = dateObj.getFullYear();
+    var hour = 0;
+    var hr = dateObj.getHours();
+    var am_or_pm;
+    var day2;
+
+    if (month < 10){
+        month = "0" + month;
+    }
+    if (day < 10){
+        day = "0" + day-1;
+        day2 = "0" + (day-3);
+    }
+
+    // PM
+    if (hr > 12){
+        hour = hr % 12 || 12;
+        am_or_pm = "PM"
+    }
+    // AM
+    else {
+        console.log('its da AM!!');
+        hour = hr % 12 || 12;
+        am_or_pm = "AM"
+    }
+    var minutes = dateObj.getMinutes();
+    var start_date = month + "/" + day2 + "/" + year + " " + hour + ":" + minutes + " " + am_or_pm;
+    var end_date = month + "/" + day + "/" + year + " " + hour + ":" + minutes + " " + am_or_pm;
+
+    var edited_date = start_date + " - " + end_date;
+    console.log(edited_date);
+    newdate = year + "/" + month + "/" + day;
+    res.render('index', { title: 'Express', date: edited_date});
 });
 
 var Flickr = require("flickrapi"),
