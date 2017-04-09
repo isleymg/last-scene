5c578c9100fa7f5a03056ec71864d4b4faf1cdf1
Isley Gao <Isley Gao>
2017-04-08 18:43:07 -0700

    deleted chatbot files:

--- src/config.js
+++
@@ -1,6 +0,0 @@
-var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAZAol337ENgBAFaNP0nhGooerwwjzMKyvZAkm19S4RAZBgDKkAcARVofjeKbnBPSYVwIPgsBJAC4jUcBda3V3sLyZBFShyNA7hhwIyOsjue2xHEeETg6Nd5zUVADPLr1WepW0Equmm60zUb1Ah8WlyxR1BkfyGR4V1uPgTwtwZDZD';
-
-module.exports = {
-  messengerPageToken: messengerPageToken,
-  messengerVerifyToken: 'TOKEN'
-};
