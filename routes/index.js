ec33d586fd892145e3a8df3eca10d2f6bd3b70c8
alyssalingad <alyssalingad@gmail.com>
2017-04-08 23:35:47 -0700

    created basic starting page and changed .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var flickr = require("flickrapi");
+// flickr key: 65655937b6264a3872c5a808c754747c
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
